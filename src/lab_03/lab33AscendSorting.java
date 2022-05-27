74197cb36762fec0940f059385bbb676c2d191e4
Le Thi Thanh Ha <105093531+haha258369@users.noreply.github.com>
2022-05-27 15:49:10 +0700

    Update lab33AscendSorting.java

--- src/lab_03/lab33AscendSorting.java
+++ src/lab_03/lab33AscendSorting.java
@@ -1,2 +1,26 @@
-package lab_03;public class lab33AscendSorting {
+package lab_03;
+
+public class lab33AscendSorting {
+
+    public static void main(String[] args) {
+
+        int[] myArr33 = new int[]{15, 0, 6, 9, 21};
+
+        for (int i1 = 0; i1 < myArr33.length; i1++) {
+            for (int i2 = i1 + 1; i2 < myArr33.length; i2++) {
+                if (myArr33[i1] > myArr33[i2]) {
+                    int a = myArr33[i1];
+                    myArr33[i1] = myArr33[i2];
+                    myArr33[i2] = a;
+                }
+            }
+        }
+
+        System.out.print("Ascending sorted array: {");
+        for (int i1 = 0; i1 < myArr33.length; i1++) {
+            System.out.print(" " + myArr33[i1]);
+        }
+        System.out.print(" }");
+
+    }
 }
