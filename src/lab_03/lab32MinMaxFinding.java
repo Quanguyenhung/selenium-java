f331fa18fa19a684e43a49448e44fbbfa2e8189d
Le Thi Thanh Ha <105093531+haha258369@users.noreply.github.com>
2022-05-27 15:48:02 +0700

    Update lab32MinMaxFinding.java

--- src/lab_03/lab32MinMaxFinding.java
+++ src/lab_03/lab32MinMaxFinding.java
@@ -1,2 +1,29 @@
-package lab_03;public class lab32MinMaxFinding {
+package lab_03;
+
+public class lab32MinMaxFinding {
+
+    public static void main(String[] args) {
+
+        int[] myArr32 = new int[]{1,1,1,1,2};
+        int minValue = myArr32[0];
+        int maxValue = myArr32[0];
+
+        for (int index = 1; index < myArr32.length; index++) {
+            if (minValue > myArr32[index]){
+                minValue = myArr32[index];
+            }
+
+            if (maxValue < myArr32[index]){
+                maxValue = myArr32[index];
+            }
+
+        }
+
+        if (maxValue == minValue){
+            System.out.println("All numbers in this array are " + minValue + ".");
+        } else {
+            System.out.println("Minimum value: " + minValue);
+            System.out.println("Maximum value: " + maxValue);
+         }
+    }
 }
